2bd30ab8c6fa629e4d69d127ef6e973bafbb1639
Brian0615 <brianps.lam@mail.utoronto.ca>
2024-12-07 22:57:26 -0500

    CNE-1: Update navigation sidebar

--- ui/main.py
+++ ui/main.py
@@ -1,110 +1,24 @@
-import base64
-import os.path
-
 import streamlit as st
-from streamlit_card import card
 
-from common.constants import DeviceType
-from src.constants import CNEDates
-from ui.src.data_service import DataService
-from ui.src.utils import display_rentals, display_reservations
-
-st.set_page_config(layout="wide")
-data_service = DataService()
-
-cols = st.columns(5, gap="small")
-card_info = [
+pages = st.navigation(
     {
-        "asset_path": "assets/new_reservation.png",
-        "page_link": "pages/reservation_form.py",
-        "key": "card1",
-    },
-    {
-        "asset_path": "assets/start_rental.png",
-        "page_link": "pages/rental_form.py",
-        "key": "card2",
-    },
-    {
-        "asset_path": "assets/modify_rental.png",
-        "page_link": "pages/manage_rental.py",
-        "key": "card3",
-    },
-    {
-        "asset_path": "assets/complete_rental.png",
-        "page_link": "pages/manage_rental.py",
-        "key": "card4",
-    },
-    {
-        "asset_path": "assets/admin_options.png",
-        "page_link": "pages/reservation_form.py",
-        "key": "card5",
-    },
-
-]
-
-for col, info in zip(cols, card_info):
-    with open(os.path.join(os.path.dirname(__file__), info["asset_path"]), "rb") as f:
-        data = f.read()
-        encoded = base64.b64encode(data)
-    data = "data:image/png;base64," + encoded.decode("utf-8")
-    with col:
-        card(
-            title="",
-            text="",
-            styles={
-                "title": {
-                    "font-size": "20px",
-                    "fontFamily": "sans-serif",
-                },
-                "text": {
-                    "font-size": "20px",
-                },
-                "card": {
-                    "width": "100%",
-                    "height": "50px",
-                    "margin": "5px 5px 5px 5px",
-                },
-                "filter": {
-                    "background-color": "rgba(0, 0, 0, 0)",
-                },
-            },
-            image=data,
-            key=info["key"],
-            on_click=lambda: st.switch_page(info["page_link"]),
-        )
-
-reservations_tab, rentals_tab = st.tabs(["Reservations", "Rentals"])
-
-with reservations_tab:
-    reservations = data_service.get_reservations_on_date(CNEDates.get_default_date())
-    if reservations.empty:
-        st.warning(
-            f"**No Reservations Today**: There are no reservations for "
-            f"{CNEDates.get_default_date().strftime('%b %d, %Y')}."
-        )
-    else:
-        scooter_reservations, wheelchair_reservations = (
-            reservations[reservations["device_type"] == DeviceType.SCOOTER],
-            reservations[reservations["device_type"] == DeviceType.WHEELCHAIR],
-        )
-        st.subheader(f"Today's {DeviceType.SCOOTER} Reservations")
-        display_reservations(scooter_reservations, device_type=DeviceType.SCOOTER)
-        st.subheader(f"Today's {DeviceType.WHEELCHAIR} Reservations")
-        display_reservations(wheelchair_reservations, device_type=DeviceType.WHEELCHAIR)
-
-with rentals_tab:
-    rentals = data_service.get_rentals_on_date(CNEDates.get_default_date())
-    if rentals.empty:
-        st.warning(
-            f"**No Rentals Today**: There are no rentals for "
-            f"{CNEDates.get_default_date().strftime('%b %d, %Y')}."
-        )
-    else:
-        scooter_rentals, wheelchair_rentals = (
-            rentals[rentals["device_type"] == DeviceType.SCOOTER],
-            rentals[rentals["device_type"] == DeviceType.WHEELCHAIR],
-        )
-        st.subheader(f"Today's {DeviceType.SCOOTER} Rentals")
-        display_rentals(scooter_rentals, device_type=DeviceType.SCOOTER)
-        st.subheader(f"Today's {DeviceType.WHEELCHAIR} Rentals")
-        display_rentals(wheelchair_rentals, device_type=DeviceType.WHEELCHAIR)
+        "Home": [
+            st.Page("ui_pages/home.py", title="Home", icon=":material/home:", default=True),
+        ],
+        "Rentals": [
+            st.Page("ui_pages/view_rentals.py", title="View Rentals", icon=":material/manage_search:"),
+            st.Page("ui_pages/manage_rental.py", title="Manage Rental", icon=":material/settings:"),
+            st.Page("ui_pages/new_rental.py", title="New Rental", icon=":material/add_circle:"),
+        ],
+        "Reservations": [
+            st.Page("ui_pages/view_reservations.py", title="View Reservations", icon=":material/manage_search:"),
+            st.Page("ui_pages/manage_reservations.py", title="Manage Reservations", icon=":material/settings:"),
+            st.Page("ui_pages/new_reservation.py", title="New Reservation", icon=":material/add_circle:"),
+        ],
+        "Inventory": [
+            st.Page("ui_pages/view_inventory.py", title="View Inventory", icon=":material/manage_search:"),
+            st.Page("ui_pages/manage_inventory.py", title="Manage Inventory", icon=":material/settings:"),
+        ],
+    }
+)
+pages.run()
